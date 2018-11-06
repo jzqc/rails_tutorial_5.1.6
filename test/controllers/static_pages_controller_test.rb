beb7314dd53227aecb735cd58652ab4287531288
jz <jzqc37@gmail.com>
2018-11-06 17:09:37 +0800

    Add | Microblog to static_pages test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,25 +9,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "Home | Microblog"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help | Microblog"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About | Microblog"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact"
+    assert_select "title", "Contact | Microblog"
   end
 
 end
