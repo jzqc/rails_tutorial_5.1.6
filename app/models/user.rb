fe016682ee7bb54d751b979b11918b7f97cf4e46
charazn <charazn37@gmail.com>
2018-11-18 14:12:01 +0800

    Add remember_token to cookies, use cookies if session does not exist (new users)

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,11 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    # BCrypt::Password.new(password_digest) == unencrypted_password
+    # BCrypt::Password.new(remember_digest) == remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
